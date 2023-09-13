c7b8561a36450e741b49fd6a7b6a135ab7b6e2b4
sHaiHoang <h.n.hai0518@gmail.com>
2023-09-13 02:55:30 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/features.css';
+import '../styles/components/projects.css';
+import '../styles/components/footer.css';
+import '../styles/utils.css';
